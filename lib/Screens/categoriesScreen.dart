4d5b795d698f87a0b923c45f5f80615af66bedea
HussamS2 <hussamips5@gmail.com>
2021-05-16 17:35:07 +0300

    ..

--- lib/Screens/categoriesScreen.dart
+++ lib/Screens/categoriesScreen.dart
@@ -47,6 +47,7 @@ class CategoriesScreen extends StatelessWidget {
           ],
         ),
       ),
+      endDrawer: Drawer(),
       body: GridView.count(
         primary: false,
         padding: const EdgeInsets.all(12),
